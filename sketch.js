11b14591511993652b567fe8fcfe39aa980aa5dd
pfeger <patrick.feger@gmail.com>
2016-01-31 17:21:41 -0500

    initial commit
    Draw smiley faces in random locations

--- sketch.js
+++ sketch.js
@@ -3,5 +3,21 @@ function setup () {
 }
 
 function draw() {
+  background(255)
+  //call function
+  createSmiley(random(0,600), random(0,600), 60, 60);
+}
 
+function createSmiley(x,y,smileySize) {
+  //mouth
+  arc(x, y+8, 25, 15, 0, PI)
+  //pupils
+  ellipse(x-10, y-7, 2, 6)
+  ellipse(x+10, y-7, 2, 6)
+  //eyes
+  ellipse(x-10, y-7, 8, 12)
+  ellipse(x+10, y-7, 8, 12)
+  //face
+  ellipse(x, y, 60, 60)
+  noFill()
 }
